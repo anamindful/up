71cb98ec6a1ee99b1905822656287f7eb6bdb64a
Sarah Carlos <SarahDCarlos@outlook.com>
2024-01-06 15:02:46 -0600

    Functional login and registration pages

---
+++ TravelUP/src/main/java/org/greencoding/TravelUP/controllers/AuthenticationController.java
@@ -0,0 +1,126 @@
+package org.greencoding.TravelUP.controllers;
+
+
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.greencoding.TravelUP.models.DTO.LoginFormDTO;
+import org.greencoding.TravelUP.models.DTO.RegistrationFormDTO;
+import org.greencoding.TravelUP.models.User;
+import org.greencoding.TravelUP.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    public User getUserFromSession(HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+
+        }
+
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isEmpty()) {
+            return null;
+        }
+
+        return userOpt.get();
+    }
+
+    @GetMapping("/createaccount")
+    public String displayRegistrationForm(Model model, HttpSession session) {
+        model.addAttribute(new RegistrationFormDTO());
+        return "createaccount";
+    }
+
+    @PostMapping("/createaccount")
+    public String processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO, Errors errors, HttpServletRequest request) {
+
+        if(errors.hasErrors()) {
+            return "createaccount";
+        }
+
+        User existingUser = userRepository.findByUsername(registrationFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyExists", "A user with that username already exists");
+            return "createaccount";
+        }
+        String password = registrationFormDTO.getPassword();
+        String verifyPassword = registrationFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            return "createaccount";
+        }
+
+        User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model, HttpSession session) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors,
+                                   HttpServletRequest request) {
+
+        // Send user back to form if errors are found
+        if (errors.hasErrors()) {
+            return "login";
+        }
+
+        // Look up user in database using username they provided in the form
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        // Get the password the user supplied in the form
+        String password = loginFormDTO.getPassword();
+
+        // Send user back to form if username does not exist OR if password hash doesn't match
+        // "Security through obscurity" — don't reveal which one was the problem
+        if (theUser == null || !theUser.isMatchingPassword(password)) {
+            errors.rejectValue(
+                    "password",
+                    "login.invalid",
+                    "Please try again with correct username/password combination."
+            );
+            return "login";
+        }
+
+        // OTHERWISE, create a new session for the user and take them to the home page
+        setUserInSession(request.getSession(), theUser);
+        return "redirect:/";
+    }
+
+    // Handler for logout
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+}
--- TravelUP/src/main/java/org/greencoding/TravelUP/models/User.java
+++ TravelUP/src/main/java/org/greencoding/TravelUP/models/User.java
@@ -1,9 +1,32 @@
 package org.greencoding.TravelUP.models;
 
-import jakarta.persistence.*;
-import  java.util.List;
-
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Entity
-@Table(name="User")
 public class User extends AbstractEntity{
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
 }
