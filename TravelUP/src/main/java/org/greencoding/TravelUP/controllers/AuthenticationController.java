8fc2329afe0202f271a0ec2b576fc17a8c47894f
Sarah Carlos <SarahDCarlos@outlook.com>
2024-01-07 20:45:30 -0600

    adding loginformDTO

--- TravelUP/src/main/java/org/greencoding/TravelUP/controllers/AuthenticationController.java
+++ TravelUP/src/main/java/org/greencoding/TravelUP/controllers/AuthenticationController.java
@@ -1,7 +1,6 @@
 package org.greencoding.TravelUP.controllers;
 
 
-import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -11,6 +10,7 @@ import org.greencoding.TravelUP.models.User;
 import org.greencoding.TravelUP.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
